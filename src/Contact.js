7a5a3c6115feef77200bf0d03f91ee96f0a19925
theoffbrandman <61510063+theoffbrandman@users.noreply.github.com>
2024-05-20 22:46:25 -0400

    Update Contact.js

--- src/Contact.js
+++ src/Contact.js
@@ -8,13 +8,11 @@ const Contact = () => {
         <h1>Contact Page</h1>
         <p>Under construction! Come back later</p>
       </div>
-
       <div className="wrapper">
         <p>No Latin here! Sorry about that...</p>
         <p>Who am I? Who are you?</p>
         <p>..................</p>
       </div>
-
       <div className="container"></div>
     </div>
   );
